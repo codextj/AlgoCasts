f9dbe53fa6b4caf2423840367bdbf88788c66214
Tanmay Jain <tanmayjain.cs@gmail.com>
2022-01-25 12:01:01 +0530

    my solutions

--- exercises/anagrams/index.py
+++ exercises/anagrams/index.py
@@ -8,3 +8,34 @@
 #   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 #   anagrams('Hi there', 'Bye there') --> False
 
+from collections import Counter
+import re
+
+def to_alphanum_lower_case_str(string):
+    return re.sub(r"[^\w]", "", string).lower()
+
+def anagrams(string_a, string_b):
+    string_a, string_b = map(to_alphanum_lower_case_str, (string_a, string_b))
+
+    if len(string_a) != len(string_b):
+        return False
+
+    return Counter(string_a) == Counter(string_b)
+
+def anagrams2(string_a, string_b):
+    string_a, string_b = map(to_alphanum_lower_case_str, (string_a, string_b))
+
+    if len(string_a) != len(string_b):
+        return False
+    
+    return sorted(string_a) == sorted(string_b)
+
+print(anagrams('rail safety', 'fairy tales'))
+print(anagrams('RAIL! SAFETY!', 'fairy tales'))
+print(anagrams('Hi there', 'Bye there'))
+print(anagrams('A tree, a life, a bench', 'A tree, a fence, a yard'))
+
+print(anagrams2('rail safety', 'fairy tales'))
+print(anagrams2('RAIL! SAFETY!', 'fairy tales'))
+print(anagrams2('Hi there', 'Bye there'))
+print(anagrams2('A tree, a life, a bench', 'A tree, a fence, a yard'))
