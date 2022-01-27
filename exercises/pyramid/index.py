ef14bbf09b25a58815d32c494bda02acf028b359
Tanmay Jain <tanmayjain.cs@gmail.com>
2022-01-27 07:04:51 +0530

    my solutions

--- exercises/pyramid/index.py
+++ exercises/pyramid/index.py
@@ -14,10 +14,29 @@
 # //       ' ### '
 # //       '#####'
 
+from platform import python_branch
+
+
 def pyramid(n):
     for i in range(n):
         hash_fill = 2*i + 1
         space_fill = ((2*n -1) - hash_fill) // 2
         print(" "*space_fill + "#"*hash_fill + " "*space_fill)
 
-pyramid(5)
+def pyramid2(n, row=0, string=""):
+    if n == row:
+        return
+    
+    if len(string) == 2*n-1:
+        print(string)
+        return pyramid2(n, row+1, "")
+
+    midpoint = (2*n-1)//2
+    if len(string) < midpoint - row or len(string) > midpoint + row:
+        string += " "
+    else:
+        string += "#"
+
+    return pyramid2(n, row, string)
+
+pyramid2(5)
